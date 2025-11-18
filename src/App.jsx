3329f32ce5776f640c1109f513c4dfb8943310b1
dana19-tech <danielle.mitu1907@gmail.com>
2025-11-18 12:26:20 +0100

    Lagt til Count-komponent med tre instanserGjort teller felles mellom alle komponenter

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
+import { useState } from "react";
 import Count from "./Components/Count";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <>
     <div>
-      <h1>Tre tellere</h1>
-      <Count />
-      <Count />
-      <Count />
+      <h1>Felles teller</h1>
+      <Count count={count} setCount={setCount} />
+      <Count count={count} setCount={setCount} />
+      <Count count={count} setCount={setCount} />
     </div>
-    </>
   );
 }
 
